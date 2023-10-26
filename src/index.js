698b2cd05e5b255efdf5ff89c93b6216227775c3
Anna Chebysheva <anna.chebysheva@gmail.com>
2023-10-26 23:32:25 +0200

    refactor: delete comments and consolelogs

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  let arr = [] //создала проверочный массив
+  let arr = []
 
-  for (let i = 0; i < str.length; i++) { //цикл по каждому символу в строке
-    let bracket = str[i] // переменная которая содержит текущий символ
-    // console.log(bracket)
-    let isOpeningBracket = bracketsConfig.some((config) => { //проверка совпадает ли текущий символ с открывающейся скобкой
+  for (let i = 0; i < str.length; i++) {
+    let bracket = str[i]
+
+    let isOpeningBracket = bracketsConfig.some((config) => {
       return config[0] === bracket
     })
-    let isClosingBracket = bracketsConfig.some((config) => { //проверка совпадает ли текущий символ с закрывающейся скобкой
+    let isClosingBracket = bracketsConfig.some((config) => {
       return config[1] === bracket
       })
-    // console.log(isOpeningBracket)
-    // console.log(isClosingBracket)
+
 
     if (isClosingBracket && isOpeningBracket) {
       if ( arr.length > 0 && arr[arr.length - 1] === `${bracket}$`) {
@@ -19,25 +18,24 @@ module.exports = function check(str, bracketsConfig) {
       } else {
         arr.push(`${bracket}$`);
       }
-      // console.log('test')
-      // console.log(arr)
+
     }
 
-    if (isOpeningBracket && !isClosingBracket) { //если это открывающаяся скобка
-      arr.push(bracket) //добавляю ее в массив
-      // console.log(arr)
+    if (isOpeningBracket && !isClosingBracket) {
+      arr.push(bracket)
+
     }
 
-    if (isClosingBracket && !isOpeningBracket) { //если это закрывающаяся скобка
+    if (isClosingBracket && !isOpeningBracket) {
 
       if (arr.length === 0) {
-        // console.log('test')
+
         return false
       } else {
-      const lastOpeningBracket = arr.pop() //удаляю из массива последний символ
-      // console.log(arr)
-      const targetOpeningBracket = bracketsConfig.find(config => config[1] === bracket)[0] //ищу в конфиге соответствующую открывающуюся скобку
-      if (lastOpeningBracket !== targetOpeningBracket) { // если скобки не совпадают возвращаю false
+      const lastOpeningBracket = arr.pop()
+
+      const targetOpeningBracket = bracketsConfig.find(config => config[1] === bracket)[0]
+      if (lastOpeningBracket !== targetOpeningBracket) { 
         return false
       }
       }
